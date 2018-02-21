26199fec858f2fa96f49beae65873e02a4ea21b7
Zachary Sunberg <sunbergzach@gmail.com>
2018-02-20 20:05:18 -0800

    added error for abs

--- src/common.jl
+++ src/common.jl
@@ -1,11 +1,13 @@
 Base.isfinite(a::AbstractVec) = all(isfinite.(a))
 Base.isinf(a::AbstractVec) = any(isinf.(a))
 
-function StaticArrays.similar_type(::Type{V}, ::Type{Float64}, size::Size) where {V<:AbstractVec}
+function StaticArrays.similar_type(::Type{V}, ::Type{F}, size::Size) where {V<:AbstractVec, F<:AbstractFloat}
     # TODO: implement this for other types
-    if size == Size(V) && eltype(V) == Float64
+    if size == Size(V) && eltype(V) == F
         return V
     else
         error("StaticArrays.similar_type is not yet implemented for Vec except for Float64 and matching size.")
     end
 end
+
+Base.abs(a::AbstractVec) = error("abs(v::AbstractVec) has been removed. Use norm(v) to get the norm; abs.(v) to get the element-wise absolute value.")

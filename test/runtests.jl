399ca35314939993f3eeb257fabf8ed2423ec57b
MaximeBouton <maxim.bouton@gmail.com>
2020-02-25 18:06:17 -0800

    remove REQUIRE, updated test, fixed isnan

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,13 +8,30 @@ import Random: seed!
 @test invlerp(0.0,1.0,0.7) ≈ 0.7
 @test invlerp(10.0,20.0,12.0) ≈ 0.2
 
+@testset "VecE2" begin
+    include("test_vecE2.jl")
+end
 
-include("test_vecE2.jl")
-include("test_vecE3.jl")
-include("test_vecSE2.jl")
-include("test_quat.jl")
+@testset "VecE3" begin
+    include("test_vecE3.jl")
+end
 
-include("test_coordinate_transforms.jl")
-include("test_geomE2.jl")
+@testset "VecSE2" begin
+    include("test_vecSE2.jl")
+end
 
-include("test_diff.jl")
+@testset "quaternions" begin
+    include("test_quat.jl")
+end
+
+@testset "coordinate transforms" begin 
+    include("test_coordinate_transforms.jl")
+end
+
+@testset "geom" begin 
+    include("test_geomE2.jl")
+end
+
+@testset "diff" begin 
+    include("test_diff.jl")
+end
